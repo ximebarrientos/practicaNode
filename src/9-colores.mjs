af3080651762b5dce23538198358a6889fdcae64
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-08-06 09:45:38 -0300

    agregamos un ejemplo de un modulo de terceros

---
+++ src/9-colores.mjs
@@ -0,0 +1,9 @@
+import colors from 'colors';
+//const colors = require('colors'); si usara con commodejs
+
+const mensaje= 'Hola estamos probando dependencies con node.js';
+
+console.log(mensaje.bgCyan)
+console.log(mensaje.rainbow)
+console.log(mensaje.green)
+console.log(mensaje.underline)
